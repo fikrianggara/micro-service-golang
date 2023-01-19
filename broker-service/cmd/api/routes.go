58a2d16dd4796192d9007ce6c935285a8cf76039
fikrianggara <fikriseptriananggara@gmail.com>
2023-01-19 15:40:36 +0700

    add broker service

---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+//router : go-chi
+//cors : go-chi/cors
+//middleware : go-chi/v5/middleware
+func (app *Config) routes() http.Handler {
+
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	//for check service life status (available or not)
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	mux.Post("/", app.Broker)
+
+	return mux
+}
